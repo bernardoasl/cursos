d651c73413e65e9e592c0347ea1aaf853fe897c3
Bernardo Lopes <bernardolopesdev@gmail.com>
2025-08-12 15:12:32 -0300

    Novo Exercicio

--- curso-javascript-hashtag/modulo_06/aula-09/script.js
+++ curso-javascript-hashtag/modulo_06/aula-09/script.js
@@ -1,4 +1,4 @@
-// Lista de funcionários de um zoológico
+// Lista de Funcionarios de um zoológico
 let funcionarios = [
   {
     nome: "João",
@@ -19,8 +19,14 @@ let funcionarios = [
     disponivel: false,
   },
   {
-    nome: "Maria",
+    nome: "Renata",
     categoria: "tratador",
+    salario: 2500,
+    disponivel: true,
+  },
+  {
+    nome: "Paulo",
+    categoria: "veterinário",
     salario: 2500,
     disponivel: false,
   },
@@ -28,7 +34,7 @@ let funcionarios = [
 
 // Listar funcionarios por categoria
 function listarFuncionariosCategoria(categoria) {
-  console.log(`Funcionário na categoria ${categoria}`);
+  console.log(`Funcionario na categoria ${categoria}`);
   for (let index = 0; index < funcionarios.length; index++) {
     if (funcionarios[index].categoria === categoria) {
       console.log(funcionarios[index].nome);
@@ -36,9 +42,29 @@ function listarFuncionariosCategoria(categoria) {
   }
 }
 
-listarFuncionariosCategoria("tratador")
+listarFuncionariosCategoria("veterinário");
 
-// Função calcular o total de gasto em salário
-function calcularSalarios() {
-    let totalSalarios = 0
+// Função calcular o total gasto em salario
+function calcularSalario() {
+  let totalSalario = 0;
+  for (let i = 0; i < funcionarios.length; i++) {
+    totalSalario += funcionarios[i].salario;
+  }
+  return totalSalario;
 }
+
+console.log(calcularSalario());
+
+// Função contar funcionários que estão disponíveis = true;
+
+function funcionariosDisponiveis() {
+  let contador = 0;
+  for (let index = 0; index < funcionarios.length; index++) {
+    if (funcionarios[index].disponivel) {
+      contador++;
+    }
+  }
+  return contador;
+}
+
+console.log(funcionariosDisponiveis());
